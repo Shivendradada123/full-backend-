fef97335d97f543d680fdda6b261d72bfa7bfe7b
Shivedra123 <shivendrasaket2580@gmail.com>
2025-08-12 08:30:21 +0530

    yes

--- day 1st/1st.js
+++ day 1st/1st.js
@@ -1,1 +1,8 @@
 console.log("mene aaj hi course liya hai aur aaj se backend start kar raha hu suru se ");
+
+
+if(user === maoderpakdu){
+    console.log("user is pakdu");
+}else{
+    console.log("user is not admin");
+}
