db6c0afb4d979f7fe10865ad1a47ab31b16bf7e2
Shivedra123 <shivendrasaket2580@gmail.com>
2025-08-31 10:47:35 +0530

    api

--- day 1st/1st.js
+++ day 1st/1st.js
@@ -1,48 +1,32 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
 
-  const la =   prompt('top') 
-    if (la === 'kismat') {
-      alert('kismat chdi hai')
-    } else {
-      console.log('kismat ')
-    }
-  
+const ACCESS_TOKEN = "yaha_apna_access_token_dalo";
 
+app.get("/profile", async (req, res) => {
+  try {
+    const url = `https://graph.instagram.com/me?fields=id,username,account_type,media_count&access_token=${ACCESS_TOKEN}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
 
+app.get("/media", async (req, res) => {
+  try {
+    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink&access_token=${ACCESS_TOKEN}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
 
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
 
 
 
