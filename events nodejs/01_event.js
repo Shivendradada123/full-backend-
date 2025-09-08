56e84ebdcc9dd9f87dc1246efaaddc8bb8d0e40c
Shivedra123 <shivendrasaket2580@gmail.com>
2025-09-08 22:20:06 +0530

    real time chat app

---
+++ events nodejs/01_event.js
@@ -0,0 +1,16 @@
+
+const EventEmitter = require('events');
+
+
+const eventEmitter = new EventEmitter();
+
+
+eventEmitter.on("nigga", (user) => {
+    console.log(`Hello ${user} nigga`);
+});
+
+
+eventEmitter.emit("nigga", "kalua");.
+
+
+con
