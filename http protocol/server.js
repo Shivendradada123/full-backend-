aba7ff61f3ed1b33d7b0b9940de0ff657bbb77cc
Shivedra123 <shivendrasaket2580@gmail.com>
2025-09-13 14:57:51 +0530

    res

--- http protocol/server.js
+++ http protocol/server.js
@@ -1,5 +1,16 @@
 const http = require('http')
 
-const server = hhtp.create
+const server = http.createServer(function (req, res) {
+    console.log('Ha reques a rahi hai ')
+    res.writeHead(200)
+    res.end('yes shivendra chal gya server aja')
+});
 
-c
+
+
+
+server.listen(8000, function () {
+    console.log(`http runnig on port 8000`)
+});
+
+con
