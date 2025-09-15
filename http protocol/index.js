c167bb5d60e80b46d73e167920447fe4468a0284
Shivedra123 <shivendrasaket2580@gmail.com>
2025-09-15 21:22:12 +0530

    log

---
+++ http protocol/index.js
@@ -0,0 +1,16 @@
+const http = require('node:http')
+
+const server = http.createServer(function (req, res) {
+
+})
+
+
+server.listen(8000, () => console.log('server is runnig 8000'))
+
+
+
+
+
+
+
+console.log('p')
