8a1f6c0a7c660ed88174906d446e0adcf633b2a9
Shivedra123 <shivendrasaket2580@gmail.com>
2025-09-16 18:06:46 +0530

    kaddu

--- http protocol/index.js
+++ http protocol/index.js
@@ -1,16 +1,24 @@
-const http = require('node:http')
+const http = require("node:http");
 
+// Create the server
 const server = http.createServer(function (req, res) {
+  // Check which URL the user is requesting
+  if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    return res.end("Aap contact page par ho.");
+  } else if (req.url === "/lalu") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    return res.end("Hello Mauhariya!");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    return res.end("Error 404: Page Not Found");
+  }
+});
 
-})
+// Server ko 8000 port par run karo
+server.listen(8000, () => {
+  console.log("Server is running at http://localhost:8000");
+});
 
 
-server.listen(8000, () => console.log('server is runnig 8000'))
-
-
-
-
-
-
-
-console.log('p')
+c
